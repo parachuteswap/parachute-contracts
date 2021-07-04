d98e7f92ccf2782038396d99d0506c8ee24099a3
parachuteswap <86835096+parachuteswap@users.noreply.github.com>
2021-07-04 20:24:43 +0800

    update max limit of deposit fee

--- contracts/MasterChefV2.sol
+++ contracts/MasterChefV2.sol
@@ -112,7 +112,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
 
     // Add a new lp to the pool. Can only be called by the owner.
     function add(uint256 _allocPoint, IBEP20 _lpToken, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner nonDuplicated(_lpToken) {
-        require(_depositFeeBP <= 10000, "add: invalid deposit fee basis points");
+        require(_depositFeeBP <= 400, "add: invalid deposit fee basis points");
         if (_withUpdate) {
             massUpdatePools();
         }
@@ -130,7 +130,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
 
     // Update the given pool's PARA allocation point and deposit fee. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
-        require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
+        require(_depositFeeBP <= 400, "set: invalid deposit fee basis points");
         if (_withUpdate) {
             massUpdatePools();
         }
